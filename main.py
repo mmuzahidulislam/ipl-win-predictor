952fae05dc2ed8e89d72cfbe8a1919be6a339ad1
Muzahidul Islam <muzahidulislam690@gmail.com>
2024-12-08 00:48:35 +0800

    update

--- main.py
+++ main.py
@@ -44,9 +44,7 @@ if st.button('Predict Probability'):
     runs_left = target-score
     balls_left = 120 - (overs*6)
     wickets = 10 - wickets_out
-    if overs>0:
-        crr = score/overs
-    else: crr=0
+    crr = score / overs if overs > 0 else 0
     rrr = (runs_left*6)/balls_left
     input_df = pd.DataFrame({'batting_team': [batting_team], 'bowling_team': [bowling_team], 'city': [selected_city],
                              'runs_left': [runs_left], 'balls_left': [balls_left], 'wickets_left': [wickets],
